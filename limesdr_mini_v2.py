36e50106265be573f959d9a970557277a787af62
Florent Kermarrec <florent@enjoy-digital.fr>
2022-04-01 09:50:50 +0200

    Add initial platform/target with input clk, leds. Simple LiteX SoC working with JTAG-UART.

---
+++ limesdr_mini_v2.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+
+#
+# This file is part of LiteX-LimeSDR-Mini-V2 project.
+#
+# Copyright (c) 2022 Florent Kermarrec <florent@enjoy-digital.fr>
+# SPDX-License-Identifier: BSD-2-Clause
+
+# Build/Use:
+# ./limesdr_mini_v2.py --build --load
+# litex_term jtag --jtag-config=openocd_limesdr_mini_v2.cfg
+
+from migen import *
+
+import limesdr_mini_v2_platform as limesdr_mini_v2
+
+from litex.build.lattice.trellis import trellis_args, trellis_argdict
+
+from litex.soc.cores.clock import *
+from litex.soc.integration.soc_core import *
+from litex.soc.integration.builder import *
+from litex.soc.cores.led import LedChaser
+
+# CRG ----------------------------------------------------------------------------------------------
+
+class _CRG(Module):
+    def __init__(self, platform, sys_clk_freq):
+        self.rst = Signal()
+        self.clock_domains.cd_sys = ClockDomain()
+
+        # # #
+
+        # Clk.
+        clk40 = platform.request("clk40")
+
+        # PLL.
+        self.submodules.pll = pll = ECP5PLL()
+        self.comb += pll.reset.eq(self.rst)
+        pll.register_clkin(clk40, 40e6)
+        pll.create_clkout(self.cd_sys, sys_clk_freq)
+
+# BaseSoC ------------------------------------------------------------------------------------------
+
+class BaseSoC(SoCCore):
+    def __init__(self, sys_clk_freq=int(80e6), toolchain="trellis", with_led_chaser=True, **kwargs):
+        platform = limesdr_mini_v2.Platform(toolchain=toolchain)
+
+        # SoCCore -----------------------------------------_----------------------------------------
+        kwargs["uart_name"] = "jtag_uart"
+        SoCCore.__init__(self, platform, sys_clk_freq, ident="LiteX SoC on LimeSDR-Mini-V2", **kwargs)
+
+        # CRG --------------------------------------------------------------------------------------
+        self.submodules.crg = _CRG(platform, sys_clk_freq)
+
+        # Leds -------------------------------------------------------------------------------------
+        if with_led_chaser:
+            leds_r = platform.request_all("led_r")
+            leds_g = platform.request_all("led_g")
+            self.submodules.leds = LedChaser(Cat(leds_r, leds_g), sys_clk_freq)
+
+# Build --------------------------------------------------------------------------------------------
+
+def main():
+    from litex.soc.integration.soc import LiteXSoCArgumentParser
+    parser = LiteXSoCArgumentParser(description="LiteX SoC on LimeSDR-Mini-V2")
+    target_group = parser.add_argument_group(title="Target options")
+    target_group.add_argument("--build",        action="store_true", help="Build bitstream.")
+    target_group.add_argument("--load",         action="store_true", help="Load bitstream.")
+    target_group.add_argument("--toolchain",    default="trellis",   help="FPGA toolchain (trellis or diamond).")
+    target_group.add_argument("--sys-clk-freq", default=80e6,        help="System clock frequency.")
+    builder_args(parser)
+    soc_core_args(parser)
+    trellis_args(parser)
+    args = parser.parse_args()
+
+    soc = BaseSoC(
+        sys_clk_freq = int(float(args.sys_clk_freq)),
+        toolchain    = args.toolchain,
+        **soc_core_argdict(args)
+    )
+    builder = Builder(soc, **builder_argdict(args))
+    builder_kargs = trellis_argdict(args) if args.toolchain == "trellis" else {}
+    builder.build(**builder_kargs, run=args.build)
+
+    if args.load:
+        prog = soc.platform.create_programmer()
+        prog.load_bitstream(builder.get_bitstream_filename(mode="sram", ext=".svf")) # FIXME
+
+if __name__ == "__main__":
+    main()
---
+++ limesdr_mini_v2_platform.py
@@ -0,0 +1,40 @@
+#
+# This file is part of LiteX-LimeSDR-Mini-V2 project.
+#
+# Copyright (c) 2022 Florent Kermarrec <florent@enjoy-digital.fr>
+# SPDX-License-Identifier: BSD-2-Clause
+
+from litex.build.generic_platform import *
+from litex.build.lattice import LatticePlatform
+from litex.build.lattice.programmer import OpenOCDJTAGProgrammer
+
+# IOs ----------------------------------------------------------------------------------------------
+
+_io = [
+    # Clk.
+    ("clk40", 0, Pins("A9"), IOStandard("LVCMOS33")),
+
+    # Leds.
+    ("led_g", 0, Pins("R16"), IOStandard("LVCMOS25"), Misc("OPENDRAIN=ON")),
+    ("led_g", 1, Pins("M18"), IOStandard("LVCMOS25"), Misc("OPENDRAIN=ON")), # Shared with FPGA_GPIO4.
+    ("led_g", 2, Pins("T17"), IOStandard("LVCMOS25"), Misc("OPENDRAIN=ON")), # Shared with FPGA_GPIO6.
+    ("led_r", 0, Pins("V17"), IOStandard("LVCMOS25"), Misc("OPENDRAIN=ON")),
+    ("led_r", 1, Pins("R18"), IOStandard("LVCMOS25"), Misc("OPENDRAIN=ON")), # Shared with FPGA_GPIO5.
+    ("led_r", 2, Pins("R17"), IOStandard("LVCMOS25"), Misc("OPENDRAIN=ON")), # Shared with FPGA_GPIO7.
+]
+
+# Platform -----------------------------------------------------------------------------------------
+
+class Platform(LatticePlatform):
+    default_clk_name   = "clk40"
+    default_clk_period = 1e9/40e6
+
+    def __init__(self, device="LFE5U", toolchain="trellis", **kwargs):
+        assert device in ["LFE5U"]
+        LatticePlatform.__init__(self, device + "-45F-8MG285C", _io, toolchain=toolchain, **kwargs)
+
+    def create_programmer(self):
+        return OpenOCDJTAGProgrammer("openocd_limesdr_mini_v2.cfg")
+
+    def do_finalize(self, fragment):
+        self.add_period_constraint(self.lookup_request("clk40", loose=True), 1e9/40e6)
